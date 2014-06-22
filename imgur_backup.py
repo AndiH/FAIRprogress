42a54c9d9c3b3cec1f969ebf7945fbbe628f39df
AndiH <a.herten+github@gmail.com>
2014-06-22 17:15:22 +0200

    Switched to new version of handling imgur.

--- imgur_backup.py
+++ imgur_backup.py
@@ -1,75 +1,11 @@
-import pyimgur
-import json
-import webbrowser
+import imgurBackup, imgurHandler
 import argparse
-import datetime
-import os
-
-
-def jsonParser(file):
-	data_file = open(file)
-	data = json.load(data_file)
-	data_file.close()
-	return data
-
-def jsonWriter(file, data):
-	with open(file, 'w') as outputfile:
-		json.dump(data, outputfile)
-
-def initializeTokens(im, credentials):
-	auth_url = im.authorization_url('pin')
-	webbrowser.open(auth_url)
-	pin = raw_input("What is the pin? ") # Python 2x
-	access_token, refresh_token = im.exchange_pin(pin)
-	credentials["access_token"] = access_token
-	credentials["refresh_token"] = refresh_token
-	return credentials
-
-def updateAccessToken(im, credentials):
-	credentials["access_token"] = im.refresh_access_token()
-	return credentials
-
-def writeToFile(file, data):
-	with open(file, 'w') as outputfile:
-		for entry in data:
-			outputfile.write(entry + "\n")
 
 def main(args):
-	imgur_credential_file = args.imgur_credentials_json
-	credentials = jsonParser(imgur_credential_file)
-	im = pyimgur.Imgur(credentials["clientid"], credentials["clientsecret"])
-	if (args.initialize):
-		credentials = initializeTokens(im, credentials)
-	if not (args.initialize):
-		im.refresh_token = credentials["refresh_token"]
-		credentials = updateAccessToken(im, credentials)
-
-	jsonWriter(imgur_credential_file, credentials)
-
+	imgur = imgurHandler.imgurHandler(args.initialize, args.imgur_credentials_json)
 	path = args.target_dir
-	if not (os.path.isabs(path)):
-		path = os.path.join(os.getcwd(), path)
-	path = os.path.normpath(path)
-	splitted_path = path.split("/")
-	# print splitted_path
-	current_day = splitted_path[-1]
-	current_month = splitted_path[-2]
-	current_year = splitted_path[-3]
-	# print current_day, current_month, current_year
-	date = datetime.date(int(current_year), int(current_month), int(current_day))
+	imgurBackup.imgurBackup(imgur, path)
 
-	album = im.create_album("FAIRprogress of " + date.strftime('%Y-%m-%d'), "Raw pictures used to generate the gif located at http://fair.andreasherten.de/images/" + current_year + "/" + current_month + "/" + current_day + ".")
-
-	jpgs_in_current_directory = [os.path.join(path, file) for file in os.listdir(path) if file.endswith(".jpg")]
-	# print jpgs_in_current_directory
-	images = []
-	for jpg in jpgs_in_current_directory:
-		images.append(im.upload_image(jpg, title=jpg.split("/")[-1]))
-	album.add_images(images)
-	
-	text = ["The individual pictures used too much space on the server's harddisk.", "We moved them.", "You can find them here: " + album.link, "Additionally, there's an offline backup.", "Just contact the author to get them!", "Cheers, Andreas"]
-
-	writeToFile(os.path.join(path, "where_are_the_pictures.txt"), text) 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Uploads pictures into albums on imgur', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
